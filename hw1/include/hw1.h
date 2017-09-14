5c36c81b8d76ea9540e01a72ff4f623937e56573
Weifeng Lin <weifelin@cs.stonybrook.edu>
2017-09-13 23:39:20 -0400

    HW 1st Sub

--- hw1/include/hw1.h
+++ hw1/include/hw1.h
@@ -2,5 +2,59 @@
 #define HW_H
 
 #include "const.h"
+//#include <stdio.h>
 
 #endif
+
+#define TRUE 1
+#define FALSE 0
+
+int string_eql(char* str1, char*str2);
+int string_to_int(char* str);
+int has_repeated_char(char* key);// return 1(TRUE) if key has repeated char and 0(FLASE) if it doesn't.
+
+
+
+//return 1(TRUE) if all letters in key are in alphabet. 0 otherwise.
+int isLegal_in_alphabet(char* key, const char* alphabet);
+
+//return EXIT_SUCCESS if no errors. EXITFAILURE otherwise.
+unsigned int cipher_pe(unsigned short mode);
+unsigned int cipher_pd(unsigned short mode);
+unsigned int cipher_fe();
+unsigned int cipher_fd();
+
+unsigned int cal_Index(unsigned int row_i, unsigned int col_i, unsigned int col);
+int is_char_in_key(char chr);
+int get_index_in_table(char chr);
+
+unsigned int char_to_Index(char chr);
+int get_morse_table_index(char chr);
+
+//add string to buffer. and return index.
+int add_to_buffer(char* buffer, char* morsed, int buffer_index);
+
+//return the number of elements in buffer.
+int buffer_size(char* buffer);
+
+//return the index after the last element.
+int shift_buffer_back3(char* buffer);
+
+int shift_buffer_back1(char* buffer);
+
+int get_frac_table_index(char* fm_buff);
+
+int get_fm_key_index(char letter);
+
+//return the index after the last element.
+int shift_buffer_backi(char* buffer, int amount);
+
+
+int string_eql_p3(char* str1, char* str2);
+
+
+int get_index_in_morse_table(char* morse_c_buff);
+
+
+
+
--- hw1/src/main.c
+++ hw1/src/main.c
@@ -15,19 +15,65 @@
 #error "Do not #include <ctype.h>. You will get a ZERO."
 #endif
 
+
 int main(int argc, char **argv)
 {
     unsigned short mode;
 
+
+
+
+
     mode = validargs(argc, argv);
+
+    //printf("MODE: 0x%X\n", mode);
 
     debug("Mode: 0x%X", mode);
 
     if(mode & 0x8000) {
         USAGE(*argv, EXIT_SUCCESS);
     }
+    if (mode == 0)
+    {
+        USAGE(*argv, EXIT_FAILURE);
+    }
 
-    return EXIT_SUCCESS;
+    //unsigned short cipher_mode = mode;
+
+    //don't care last 13 bits since "mode" contains the info and will be used if needed.
+    // pd/pe/fd/fe are just used to determine which function should be called.
+
+    unsigned short pd = 0x2000; //0x2000 = 0010 0000 0000 0000. use AND.
+    unsigned short pe = 0x0000;
+    unsigned short fd = 0x6000;//0x6000 = 0110 0000 0000 0000.
+    unsigned short fe = 0x4000;//0x4000 = 0100 0000 0000 0000.
+
+    // AND with 1110 0000 0000 0000(0XE000) to preserve first 3 bits.
+
+    unsigned int return_result = EXIT_SUCCESS;
+
+    if ((mode & 0XE000) == pe)
+    {
+        return_result = cipher_pe(mode);
+    }
+
+    if ((mode & 0xE000) == pd)
+    {
+        return_result = cipher_pd(mode);
+    }
+
+    if ((mode & 0xE000) == fe)
+    {
+        return_result = cipher_fe();
+    }
+
+    if ((mode & 0xE000) == fd)
+    {
+        return_result = cipher_fd();
+    }
+
+    //return EXIT_SUCCESS;
+    return return_result;
 }
 
 /*
